89217568c22bcaab80a2fca0d310dd9164869949
pj perry <pjperry28@gmail.com>
2022-07-22 14:41:37 -0700

    pass test for getting description

--- PierresVending/Controllers/HomeController.cs
+++ PierresVending/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ namespace HomeController.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/")]
-    public ActionResult Index()
-    {
-      Vendor starterItem = new Vendor("Add first item to To Do List");
-      return View(starterItem);
-    }
+    // [Route("/")]
+    // public ActionResult Index()
+    // {
+    //   Vendor starterItem = new Vendor("Add first item to To Do List");
+    //   return View(starterItem);
+    // }
 
   }
 }
--- PierresVending/Models/vendor.cs
+++ PierresVending/Models/vendor.cs
@@ -4,24 +4,10 @@ namespace PierresVending.Models
 {
   public class Vendor
   {
-    public string Description { get; set; }
-    private static List<Vendor> _instances = new List<Vendor> {};
-
-    public Vendor (string description)
+    public string Description {get; set;}
+    public Vendor(string description)
     {
-      Description = description;
-      _instances.Add(this);
+     Description = description;
     }
-
-    public static List<Vendor> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
   }
 }
